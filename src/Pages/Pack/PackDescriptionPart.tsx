370f3e09cc9ab7aaa86ee9995d908ae9672013de
seokmin <essemfly@gmail.com>
2021-07-18 08:20:46 +0900

    Add buy button

--- src/Pages/Pack/PackDescriptionPart.tsx
+++ src/Pages/Pack/PackDescriptionPart.tsx
@@ -1,36 +1,54 @@
 import styled from 'styled-components';
-import { Pack } from "../../Models/Card"
-import AvatarComponent from "../../Components/Avatar";
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { Pack } from '../../Models/Card';
+
+import AvatarComponent from '../../Components/Avatar';
 
 interface PackInfoProps {
-    pack: Pack;
+  pack: Pack;
 }
 
 const CreaterDiv = styled.div`
-    display: inline-block;
-    margin-right: 20px;
-`
+  display: inline-block;
+  margin-right: 20px;
+`;
 
-const PackDescriptionPart: React.FC<PackInfoProps> = ({ pack }): JSX.Element => {
-    return <div>
-        <div>
-            <h2>{pack.name}</h2>
-            가격: {pack.price}
-        </div>
-        <br />
-        <pre>
-            {pack.description}
-        </pre>
-        <h4>creaters</h4>
-        <div>
-            {pack.creaters.map(function avatarCreator(creater) {
-                return <CreaterDiv key={creater.id}>
-                    <p>{creater.name}</p>
-                    <AvatarComponent avatarUrl={creater.avatarUrl} />
-                </CreaterDiv>
-            })}
-        </div>
+const PackDescriptionPart: React.FC<PackInfoProps> = ({
+  pack,
+}): JSX.Element => {
+  const history = useHistory();
+  const handleBuyClick: React.MouseEventHandler = () => {
+    history.push('/open');
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>{pack.name}</h2>
+        가격: {pack.price}
+      </div>
+      <br />
+      <div>
+        <Button variant="outlined" color="primary" onClick={handleBuyClick}>
+          구매하기
+        </Button>
+      </div>
+      <br />
+      <pre>{pack.description}</pre>
+      <h3>Creaters</h3>
+      <div>
+        {pack.creaters.map(function avatarCreator(creater) {
+          return (
+            <CreaterDiv key={creater.id}>
+              <p>{creater.name}</p>
+              <AvatarComponent avatarUrl={creater.avatarUrl} />
+            </CreaterDiv>
+          );
+        })}
+      </div>
     </div>
-}
+  );
+};
 
-export default PackDescriptionPart
+export default PackDescriptionPart;
--- src/Routes/PackInfoPage.tsx
+++ src/Routes/PackInfoPage.tsx
@@ -1,28 +1,29 @@
 import { Grid } from '@material-ui/core';
 
-import { samplePack } from "../Models/Mockdata";
-import PackIncludePart from "../Pages/Pack/PackIncludePart";
-import PackImagePart from "../Pages/Pack/PackImagePart";
-import PackDescriptionPart from "../Pages/Pack/PackDescriptionPart";
-
+import { samplePack } from '../Models/Mockdata';
+import PackIncludePart from '../Pages/Pack/PackIncludePart';
+import PackImagePart from '../Pages/Pack/PackImagePart';
+import PackDescriptionPart from '../Pages/Pack/PackDescriptionPart';
 
 const PackInfoPage: React.FC = (): JSX.Element => {
-  return <div>
-    <Grid
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="flex-start"
-    >
-      <Grid item xs={8}>
-        <PackImagePart imageUrl={samplePack.imageUrl.toString()} />
+  return (
+    <div>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Grid item xs={8}>
+          <PackImagePart imageUrl={samplePack.imageUrl.toString()} />
+        </Grid>
+        <Grid item xs={4}>
+          <PackDescriptionPart pack={samplePack} />
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <PackDescriptionPart pack={samplePack} />
-      </Grid>
-    </Grid>
-    <PackIncludePart cardInfos={samplePack.cardInfos} />
-  </div>;
+      <PackIncludePart cardInfos={samplePack.cardInfos} />
+    </div>
+  );
 };
 
 export default PackInfoPage;
